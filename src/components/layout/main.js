c6ecdbd35fb3ef90cbee44915fb27c1499ab9ee0
Christian Colón <christian.p.colon2@outlook.com>
2020-08-13 20:48:33 -0400

    Changes to the UI libraries, animations applied

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -1,42 +1,54 @@
 import React, { PureComponent } from "react";
-
 import { connect } from "react-redux";
-
 import {
 	SafeAreaView,
 	StyleSheet,
 	ScrollView,
-	View,
 	StatusBar,
+	Animated,
+	Easing
   } from 'react-native';
 
-import { ListItem } from 'react-native-elements';
-  
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
+import { ListItem, Left, Body, Right, Icon, Text, Thumbnail, Badge } from "native-base";
+import { View } from 'react-native-animatable';
 import PropTypes from 'prop-types';
-
 import Appbar from '../shared/appbar';
-
 import { getDataFeed } from "../../actions/main";
-
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, Button, Colors, Avatar } from 'react-native-paper';
 
 const initialState = {
 	query: "",
-	filteredDataFeed: null
+	filteredDataFeed: null,
+	animateItem : new Animated.Value(0)
 };
 
 class Main extends PureComponent {
 
 	state = {...initialState}
-	
+
 	componentDidMount = async()=>{
 		this.props.getDataFeed();
+		Animated.timing(this.state.animateItem, {
+			toValue: 1,
+			duration: 1000,
+			delay: 200,
+			easing: Easing.bounce,
+			useNativeDriver: true
+		}).start()
 	}
 
+	transformY() {
+		return {
+		  translateY: this.state.animateItem.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [700, 300, 0]
+		  })
+		}
+	  }
+
 	onChangeSearch = query => {
-		 const filteredDataFeed = this.props.dataFeed.filter(entry => {    
+		let filteredDataFeed = [];
+		filteredDataFeed = this.props.dataFeed.filter(entry => {    
 			//improve  
 			const itemData = `${entry['im:name']['label'].toUpperCase()} ${entry['im:artist']['label'].toUpperCase()}`;
 			const queryData = query.toUpperCase();
@@ -47,21 +59,40 @@ class Main extends PureComponent {
 
 	render() {
 
-		let { filteredDataFeed, query } = this.state;
+		//apply loading
+		let { loading, filteredDataFeed, query } = this.state;
 		let { dataFeed } = this.props;
 		let dataSource = filteredDataFeed == null ? dataFeed : filteredDataFeed;
 
 		const listItems = dataSource.map((entry, i) => {
-			return <ListItem
-				key={i}
-				leftAvatar={{ source: { uri: entry['im:image'][2]['label'] } }}
-				onPress={() => this.props.navigation.navigate("Selected")}
-				title={entry['im:name']['label']}
-				subtitle={entry['im:artist']['label']}
-				bottomDivider
-				chevron
-				badge={{ value: '#'+(i+1), status:'warning', textStyle: { color: 'black', fontSize: 17, lineHeight: 20 } }}
-			/>
+			var start = Date.now();
+			return (
+				<Animated.View
+					key={i}
+					style={{
+					width: '100%',
+					transform: [this.transformY()]
+					}}>
+					<ListItem
+					key={i}
+					icon
+					style={{ height: 70 }}
+					onPress={() => this.props.navigation.navigate("Selected", { selected: entry })}>
+						<Left>
+							<Thumbnail square source={{uri: entry['im:image'][2]['label']}} />
+						</Left>
+						<Body style={{borderBottomWidth:0}}>
+							<Text style={{ fontSize: 14}}>{entry['im:name']['label']}</Text>
+							<Text style={{ fontSize: 10}}>{entry['im:artist']['label']+ " Time: " + start}</Text>
+						</Body>
+						<Right style={{borderBottomWidth:0, height: 70}}>
+							<Badge style={{ backgroundColor: 'yellow' }}>
+								<Text style={{ color: 'purple', fontSize: 17 }}>{'#'+(dataFeed.indexOf(entry)+1)}</Text>
+							</Badge>
+						</Right>
+					</ListItem>
+				</Animated.View>
+			);
 		});
 
 		return (
@@ -71,17 +102,18 @@ class Main extends PureComponent {
 					navigation={this.props.navigation}
 					title="Top 100"
 				/>
+				<Button
+					icon="refresh"
+					//try to unload list items prior to getting again
+					onPress={() => this.props.getDataFeed()}/>
 				<Searchbar
 				placeholder="Search"
 				onChangeText={text => this.onChangeSearch(text)}
 				value={query}/>
 				<SafeAreaView>
 					<ScrollView
-					contentInsetAdjustmentBehavior="automatic"
 					style={styles.scrollView}>
-					<View style={styles.body}>
-						{ listItems }
-					</View>
+						{listItems}
 					</ScrollView>
 				</SafeAreaView>
 			</View>
@@ -96,6 +128,7 @@ Main.propTypes = {
 const mapStateToProps = (state, ownProps) => {
 	let filtered = Array.from(state.main.dataFeed);
 	return {
+		loading: state.main.loading,
 		dataFeed: state.main.dataFeed,
 		filteredDataFeed : filtered,
 		query: "",
@@ -109,12 +142,10 @@ const mapDispatchToProps = {
 
 const styles = StyleSheet.create({
 	scrollView: {
-	  backgroundColor: Colors.lighter,
+		backgroundColor: Colors.lighter,
 	},
 	body: {
-	  backgroundColor: Colors.white,
-	  height: '100%',
-      display: 'flex',
+		backgroundColor: Colors.white,
 	},
   });
 
--- src/components/selected.js
+++ src/components/selected.js
@@ -1,15 +1,13 @@
 import React, { PureComponent } from "react";
-    
 import {
     StyleSheet,
-    View
+	View,
+	Text,
 } from 'react-native';
-
 import { connect } from "react-redux";
 import Appbar from './shared/appbar';
-import { BottomNavigation, Text} from 'react-native-paper';
-import { Icon } from 'react-native-elements';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { BottomNavigation} from 'react-native-paper';//remove, simplify with 1 UI library if possible
+import { Icon } from 'react-native-elements';//use native-base
 
 const initialState = {
     index: 0,
@@ -17,37 +15,51 @@ const initialState = {
         { key: 'info', title: 'info', icon: 'account-box' },
         { key: 'charts', title: 'charts', icon: 'format-list-numbered' },
         { key: 'library', title: 'library', icon: 'library-music' },
-    ],
+	],
 };
-
-const InfoRoute = () => <Text>InfoRoute</Text>;
-const LibraryRoute = () => <Text>MusicRoute</Text>;
-const ChartsRoute = () => <Text>ChartsRoute</Text>;
 
 class Selected extends PureComponent {
 
 	state = {...initialState}
 
+	//continue template
+	InfoRoute = () => {
+		const { selected } = this.props;
+		return <Text>InfoRoute for {selected['im:name']['label']}</Text>;
+		//details regarding the album artist
+	};
+	
+	//continue template
+	LibraryRoute = () => {
+		const { selected } = this.props;
+		return <Text>LibraryRoute for {selected['im:name']['label']}</Text>;
+		//details regarding the selected album
+	};
+
+	//continue template, comparing chart scores from 3 API feeds (GooglePlay, iTunes and Spotufy)
+	ChartsRoute = () => {
+		const { selected } = this.props;
+		return <Text>ChartsRoute for {selected['im:name']['label']}</Text>;
+	};
+
     handleIndexChange = index => this.setState({ index });
 
 	renderScene = BottomNavigation.SceneMap({
-		info: InfoRoute,
-        library: LibraryRoute,
-        charts: ChartsRoute
+		info: this.InfoRoute,
+		library: this.LibraryRoute,
+		charts: this.ChartsRoute,
 	});
 
-	renderIcon = ({route, focused = false, color }) => {
-        return <Icon color="#4A64AA" name={route.icon} size={28}/>;
+	renderIcon = ({route, focused= false, color }) => {
+		const { selected } = this.props;
+		return <Icon color="#4A64AA" name={route.icon} size={28} />
 	};
 
-	renderLabel = ({route, focused = false, color }) => {
-		return <Text style={{color:"#4A64AA", alignSelf:"center"}}  >{route.title}</Text>
+	renderLabel = ({route, focused= false, color }) => {
+		return <Text style={styles.renderLabel}  >{route.title}</Text>
 	};
     
 	render() {
-
-        let { index } = this.state;
-        
 		return (
 			<View style={styles.root}>
 				<Appbar 
@@ -57,13 +69,12 @@ class Selected extends PureComponent {
                     hasMenu={true}
 				/>
 				<BottomNavigation
-                    index={index}
 					navigationState={this.state}
 					onIndexChange={this.handleIndexChange}
 					renderScene={this.renderScene}
 					renderIcon={this.renderIcon}
 					renderLabel={this.renderLabel}
-					barStyle={{ paddingTop: 5,  paddingBottom: 20, backgroundColor:"#FFFFFF",}}
+					barStyle={styles.BottomNavigationBarStyle}
 				/>
 		   </View>
 		);
@@ -72,76 +83,35 @@ class Selected extends PureComponent {
 
 const mapStateToProps = (state, ownProps) => {
 	return {
-		propMessage: ownProps.ownMessage,
+		selected: ownProps.route["params"]["selected"],
 		navigation: ownProps.navigation
 	};
 };
 
 const mapDispatchToProps = {
-    //...
+    //...load more metadata
 };
 
 const styles = StyleSheet.create({
    root : { 
         flex: 1, 
         flexDirection: 'column',
-    },
-    container: {
-		backgroundColor: 'red',
-		flex: 1,
-		height: '600px'
 	},
-    top: {
-		left: 0,
-		right: 0,
-		top: 0,
-    },
-    bottom: {
-        alignSelf: 'center', 
-		position:"absolute",
-        bottom: 0,
-		left: 0,
-		right: 0,
-    },
-    charts: {
-		position:"absolute",
-        alignSelf: 'center', 
-        backgroundColor: '#ffffff', 
-		borderColor: '#4A64AA', 
-        width: 70, 
-		height: 73, 
-		borderWidth: 2,
-        borderRadius: 35, 
-        bottom: 25,
-        zIndex: 10 
-    },
-	container: {
-		backgroundColor: 'white'
+	BottomNavigationBarStyle:{
+		paddingTop: 5,
+		paddingBottom: 25,
+		backgroundColor:"#FFFFFF",
 	},
-	rowText: {
-		color: '#4A64AA',
-		marginLeft: 25,
-		padding: 20,
-		fontSize: 18,
-		fontWeight: "100"
+	renderLabel:{
+		color:"#4A64AA",
+		alignSelf:"center"
 	},
-	bottomNavigation: {
-        paddingBottom: 40,
-        width: '100%'
-	},
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    body: {
-        backgroundColor: Colors.white,
-        maxHeight: 500,
-    },
     info: {
     },
     library: {
     },
     chart:{
-    },
+	},
 });
 
 export default connect(
